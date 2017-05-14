8331d17febf39b51f68283db21fe32d1ab997e00
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-05-14 22:34:31 +0100

    updates to Register Controller test to add in failure and success tests

--- test/integration/controllers/RegisterController.test.js
+++ test/integration/controllers/RegisterController.test.js
@@ -2,14 +2,49 @@ var request = require('supertest');
 
 describe('RegisterController', function() {
 
-  describe('/register', function() {
-    it('should redirect to /register', function (done) {
-      request(sails.hooks.http.app)
-        .post('/register')
-        .send({ name: 'test', email: 'test@test.com' })
-        .expect(200)
-        .expect('location','/register/successful', done);
+    describe('/register successful test', function() {
+        it('should redirect to /register', function (done) {
+
+            var randomString = Math.random().toString(36).substring(3);
+
+            request(sails.hooks.http.app)
+            .post('/register')
+            .send({ 
+                name: `Test_${randomString}`, 
+                email: `testemail_${randomString}@test.com` 
+            })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect('location','/register', done);
+        });
     });
-  });
+
+    describe('/register failure test', function() {
+        var randomString = Math.random().toString(36).substring(3);
+
+        it('should redirect to /register and return 200 response code', function (done) {
+            request(sails.hooks.http.app)
+            .post('/register')
+            .send({ 
+                name: `Test_${randomString}`, 
+                email: `testemail_${randomString}@test.com` 
+            })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect('location','/register', done);
+        });
+
+        it('should redirect to /register and return 403 response code', function (done) {
+            request(sails.hooks.http.app)
+            .post('/register')
+            .send({ 
+                name: `Test_${randomString}`, 
+                email: `testemail_${randomString}@test.com` 
+            })
+            .expect(403)
+            .expect('Content-Type', /json/)
+            .expect('location','/register', done);
+        });
+    });
 
 });
