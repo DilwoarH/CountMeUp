f5d324061801ec7f8f862df95c03b194d9b047c1
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-05-15 00:04:27 +0100

    adds in VoteController tests

---
+++ test/integration/controllers/VoteController.test.js
@@ -0,0 +1,144 @@
+var request = require('supertest');
+
+describe('VoteController', function() {
+
+    describe('/vote successful test', function() {
+        
+        var randomString = Math.random().toString(36).substring(3);
+       
+        var user = {
+            name:       `Test_${randomString}`,
+            email:      `testemail_${randomString}@test.com`,
+            password:   `fdsfds324432fsfds${randomString}dsadsadsads432432234adasdas`
+        };
+
+        var candidate_data = {
+            id: 1,
+            test: true
+        };
+
+        it('POST /register', function (done) {
+
+            request(sails.hooks.http.app)
+            .post('/register')
+            .send({ 
+                name: user.name, 
+                email: user.email
+            })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect('location','/register', done);
+            
+        });
+
+        it('POST /login', function (done) {
+            
+            request(sails.hooks.http.app)
+            .post('/login')
+            .send({ 
+                email: user.email,
+                password: user.password
+            })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect('location','/', done);
+            
+        });
+
+        it('POST /vote', function (done) {
+            
+            request(sails.hooks.http.app)
+            .post('/vote')
+            .send({ 
+                candidate_id: candidate_data.id
+            })
+            .expect(201)
+            .expect('Content-Type', /json/)
+            .expect('location','/', done);
+            
+        });
+
+    });
+
+    describe('/vote failure test', function() {
+        
+        var randomString = Math.random().toString(36).substring(3);
+       
+        var user = {
+            name:       `Test_${randomString}`,
+            email:      `testemail_${randomString}@test.com`,
+            password:   `fdsfds324432fsfds${randomString}dsadsadsads432432234adasdas`
+        };
+
+        var candidate_data = {
+            id: 1,
+            test: true
+        };
+
+        it('POST /register', function (done) {
+
+            request(sails.hooks.http.app)
+            .post('/register')
+            .send({ 
+                name: user.name, 
+                email: user.email
+            })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect('location','/register', done);
+            
+        });
+
+        it('POST /login', function (done) {
+            
+            request(sails.hooks.http.app)
+            .post('/login')
+            .send({ 
+                email: user.email,
+                password: user.password
+            })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect('location','/', done);
+            
+        });
+
+        it('POST /vote', function (done) {
+            
+            //first vote
+            request(sails.hooks.http.app)
+            .post('/vote')
+            .send( candidate_data )
+            .expect(201)
+            .expect('Content-Type', /json/)
+            .expect('location','/', done);
+
+            //second vote
+            request(sails.hooks.http.app)
+            .post('/vote')
+            .send( candidate_data )
+            .expect(201)
+            .expect('Content-Type', /json/)
+            .expect('location','/', done);
+
+            //third vote
+            request(sails.hooks.http.app)
+            .post('/vote')
+            .send( candidate_data )
+            .expect(201)
+            .expect('Content-Type', /json/)
+            .expect('location','/', done);
+            
+            //fourth vote
+            request(sails.hooks.http.app)
+            .post('/vote')
+            .send( candidate_data )
+            .expect(403)
+            .expect('Content-Type', /json/)
+            .expect('location','/', done); 
+
+        });
+
+    });
+
+});
